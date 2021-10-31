2978fa37893066a7cbe87939abe210047dcb7139
Benjamin Papac <47900153+benpapac@users.noreply.github.com>
2021-10-31 16:34:50 -0700

    continue debug Resume

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Document } from 'react-pdf/dist/esm/entry.webpack';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import resume from '../../assets/_Ben Papac Dev Resume.pdf';
 
 const Resume = () => {
     return (
         <>
-            <Document file={resume} />
+            <Document file={resume} >
+                <Page pageNumber={1}></Page>
+            </Document>
         </>
     );
 };
