c469fd4103569c5122b09199f417993fb3e78820
Benjamin Papac <47900153+benpapac@users.noreply.github.com>
2021-11-01 09:12:50 -0700

    add download for resume

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -4,11 +4,14 @@ import resume from '../../assets/_Ben Papac Dev Resume.pdf';
 
 const Resume = () => {
     return (
+        <>
+        <a href="../../assets/_Ben Papac Dev Resume.pdf" download>Download</a>
         <div className="pdf">
             <Document file={resume} >
                 <Page pageNumber={1}></Page>
             </Document>
         </div>
+        </>
     );
 };
 
